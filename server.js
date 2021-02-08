c786a06017f18f260c25fc07a72bbae07735195a
Andreas Ottosson <andreasottosson@me.com>
2021-02-08 21:40:52 +0100

    Added stores, needs work on logic and threads

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
-// Import all store logic here
-import inet from "./stores/inet.js";
+import snyper from "./snyper.js";
 
-// Import all URLs here
-import inet_cards from "./cards/inet_cards.js"
+// Import all stores here
+import inet from "./stores/inet.js";
+import komplett from "./stores/komplett.js";
+import webhallen from "./stores/webhallen.js";
+
+process.setMaxListeners(0);
 
 // Print start message
-console.log("GPU Snyper starting...")
+console.log("GPU Snyper starting...");
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // Run all checks
-for (const card of inet_cards) {
-    await sleep(5000);
-    inet(card);
+while (true) {
+
+  let current_sleep;
+  current_sleep = 3000;
+
+  let webhallen_count = 0;
+  let inet_clound = 0;
+  let komplett_count = 0;
+
+  for (const card of webhallen.cards) {
+    // Randomize sleep & "chunk"
+    await sleep(current_sleep);
+    snyper("WEBHALLEN", card, webhallen.selectors);
+    webhallen_count++;
+  }
+  for (const card of inet.cards) {
+    // Randomize sleep & "chunk"
+    await sleep(current_sleep);
+    snyper("INET", card, inet.selectors);
+    inet_count++;
+  }
+  for (const card of komplett.cards) {
+    // Randomize sleep & "chunk"
+    await sleep(current_sleep);
+    snyper("KOMPLETT", card, komplett.selectors);
+    komplett_count++;
+  }
+
 }
